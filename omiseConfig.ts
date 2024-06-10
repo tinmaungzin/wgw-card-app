3e5d7b6d34720a08c0440498ae3e46466cd9cd29
Tin Maung Zin <tinmaungzin.tmz@gmail.com>
2024-06-10 11:10:27 +0700

    dependencies update

--- omiseConfig.ts
+++ omiseConfig.ts
@@ -1,6 +1,8 @@
 // @ts-ignore
 import Omise from "omise-react-native";
 import { OMISE_PUBLIC_KEY, OMISE_SECRET_KEY } from "@env";
+// console.log('OMISE_PUBLIC_KEY', OMISE_PUBLIC_KEY)
+// console.log('OMISE_SECRET_KEY', OMISE_SECRET_KEY)
 
 Omise.config(OMISE_PUBLIC_KEY, OMISE_SECRET_KEY, "2015-11-17");
 
